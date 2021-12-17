005c432bfab47b1bc47fe56bc7aef89504b1e75e
Cade Walton <waltoncade@gmail.com>
2021-12-16 19:42:31 -0800

    Client successfully sending data to server

--- client/client.c
+++ client/client.c
@@ -6,33 +6,47 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
+typedef struct {
+    int intvalue;
+    char charvalue;
+    float floatvalue;
+} dataPacket;
+
 int main(){
-    printf("client starting");
     int sock;
-    struct sockaddr_in addr;
+    struct sockaddr_in serverAddr;
+    dataPacket clientData;
     char buf[80];
+    char SYN[] = "SYN";
     sock = socket(AF_INET, SOCK_STREAM, 0); // setting up socket. AF_INET = IP, SOCK_STREAM = TCP, 0 = Auto setup
     if (sock==-1)
     {
-        perror("opening socket"); //error opening socket (print error)
+        printf("opening socket"); //error opening socket (print error)
         exit(-1);
     }
 
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(32351); //makes sure port is stored in big-endian
-    addr.sin_addr.s_addr = inet_network("10.130.26.28"); //stores ip string in big-endian
-    if(connect(sock, (const struct sockaddr *) &addr, sizeof(struct sockaddr_in))==-1);
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(32351); //makes sure port is stored in big-endian
+    serverAddr.sin_addr.s_addr = inet_network("1.1.0.127"); //stores ip string in big-endian
+
+    if(connect(sock, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0)
     {
-        perror("on connect");
+        printf("on connect\n");
         exit(-1);
     }
 
-    send(sock, "client calling server, do you read me", 38, 0);
+    clientData.intvalue = 5;
+    clientData.charvalue = 'C';
+    clientData.floatvalue = 3.14;
+
+    // send(sock, SYN, sizeof(SYN), 0);
+    printf("Data: %d", clientData.intvalue);
+    send(sock, (dataPacket*) &clientData, sizeof(clientData), 0);
     recv(sock, buf, 80, 0);
 
     if(shutdown(sock, 1)==-1)
     {
-        perror("on shutdown");
+        printf("on shutdown");
         exit(-1);
     }
     printf("Client is done\n");
--- server/server.c
+++ server/server.c
@@ -6,33 +6,39 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
+typedef struct {
+    int intvalue;
+    char charvalue;
+    float floatvalue;
+} dataPacket;
 
 int main()
 {
 	int sock, clientsock, mlen, addrsize, msgct, chc, chct;
-	struct sockaddr_in addr;
-	char ch,buf[80];
+	struct sockaddr_in serverAddr, clientAddr;
+	dataPacket serverData;
+	char ch, buf[80];
 
-	printf("server is running");
+	printf("Server is running\n\n");
 	//create a socket
-	sock = socket(AF_INET, SOCK_STREAM,0);
+	sock = socket(AF_INET, SOCK_STREAM, 0);
 	//socket doesn't exit
 	if(sock == -1)
 	{
-		perror("openning socket");
+		printf("ERROR: opening socket");
 		exit(-1);
 	}
 	/*
 	* bind a name to the socket since the server will bind with any client
 	*the address is zero or INADDR_ANY
 	*/
-	addr.sin_family =AF_INET;
-	addr.sin_port = htons(32351);
-	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serverAddr.sin_family =AF_INET;
+	serverAddr.sin_port = htons(32351);
+	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	if(bind(sock,&addr, sizeof(struct sockaddr_in)) == -1)
+	if(bind(sock, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1)
 	{
-		perror("on bind");
+		printf("ERROR: on bind");
 		exit(-1);
 	}
 
@@ -45,20 +51,32 @@ int main()
 	//wait for a client to connect. Once connected print out a message.
 
 	addrsize = sizeof(struct sockaddr_in); 
-	clientsock = accept(sock, &addr, &addrsize);
+	clientsock = accept(sock, (struct sockaddr*)&clientAddr, &addrsize);
 	if(clientsock == -1)
 	{
 		perror("on accept");
 		exit(-1);
 	}
-	printf("connection made with client %s", inet_ntoa(addr.sin_addr));
+	printf("Connection made with client %s\n", inet_ntoa(clientAddr.sin_addr));
 	
 	//receive and print a client message where a null char terminates 
 	//note a single receive may not work in some cases, but is OK for a simple 
 	//example
-	mlen = recv(clientsock,buf,80,0);
-	send(clientsock, "Got your message", 17,0);
+	mlen = recv(clientsock, (dataPacket*)&serverData, sizeof(serverData), 0);
+	send(clientsock, "Got your message", 17, 0);
 	printf("Server - all messages read - connection being closed\n");
+	printf("Message Received: %c\n", serverData.charvalue);
+	printf("Message Received: %d\n", serverData.intvalue);
+	printf("Message Received: %f\n", serverData.floatvalue);
+
+	// int l;
+	// while (1)
+	// {
+	// 	l = recv(clientsock, buf, sizeof(buf), 0);
+	// 	if (l > 0){
+	// 		printf("%s", buf);
+	// 	}
+	// }
 
 	//close the client socket and also the server socket
 
